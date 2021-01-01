21186fcce2139909d58c462ebc254a800f70f1e0
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-01 19:05:05 +0000

    init

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        
+    ]);
+
+    return ( 
+        <div className="home">
+
+        </div>
+     );
+}
+ 
+export default Home;
