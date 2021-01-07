c1bf3e1a4ab76f2dc502f5c00329a9e3d05fd938
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-07 18:08:35 +0000

    useEffect Depenencies

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 } 
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -16,14 +18,16 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
 
     return ( 
         <div className="home">
             <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>Change name</button>
+            <p>{ name }</p>
         </div>
-     );
+    );
 }
  
 export default Home;
