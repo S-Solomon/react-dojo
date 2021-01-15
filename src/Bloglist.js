ed57fcc3b2174a318c4c5b3459deae0c0911476a
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-15 14:58:04 +0000

    handling fetch errors

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,11 +1,8 @@
 
-const Bloglist = ({ blogs, title}) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
+const Bloglist = ({ blogs }) => {
 
     return ( 
         <div className="blog-list">
-        <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
