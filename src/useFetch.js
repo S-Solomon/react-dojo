02d342ecb84c68cca4e25d76d094974ac39472ce
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-18 17:13:19 +0000

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,11 @@ const useFetch = (url) => {
     const [error , setError] = useState(null);
 
     useEffect(() => {
+        const abortCont = new AbortController();
+
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if(!res.ok) { // error coming back from server
                     throw Error('could not fetch the data for that resource');
@@ -20,11 +23,19 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
-                // auto catches network / connection error
-                setIsPending(false)
-                setError(err.message);
+                if (err.name === 'AbortError') {
+                    console.log('aborted')
+                } else {
+                  // auto catches network / connection error
+                    setIsPending(false);
+                    setError(err.message);
+                }
+    
             });
         }, 1000);
+
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error}
