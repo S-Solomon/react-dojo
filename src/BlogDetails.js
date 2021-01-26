f16dd9bc2656dd8b82915f93fd3ab3e806239ad6
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-01-26 18:43:42 +0000

    Deleting Blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const { data: blog, error, isPending } = useFetch('../data/db.json' + id);
+    // http://localhost:8000/blogs/
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('../data/db.json' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -15,11 +25,12 @@ const BlogDetails = () => {
                 <h2>{blog.title}</h2>
                 <p>Written by { blog.author }</p>
                 <div>{ blog.body }</div>
+                <button onClick={handleClick}>delete</button>
             </article>
         )}
 
         </div>
      );
 }
- 
+
 export default BlogDetails;
